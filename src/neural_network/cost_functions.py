c2f3249b449de18bdfd32962728c07adc303da7e
Felipe Adeildo <felipeadeildo@duck.com>
2024-07-06 17:11:17 -0300

    feat: add cost functions implementation

---
+++ src/neural_network/cost_functions.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+
+class CostFunctions:
+    """
+    Cost Functions to be used in the neural network
+
+    Methods:
+        cross_entropy(y_hat, y)
+        mean_squared_error(y_hat, y)
+    """
+
+    @staticmethod
+    def cross_entropy(y_hat: np.ndarray, y: np.ndarray) -> float:
+        """
+        Compute the Cross-Entropy Loss function.
+
+        The cross-entropy loss is defined as:
+
+        L = - (1/n) Σ Σ y_{ij} log(y_hat_{ij})
+
+        Args:
+            y_hat (np.ndarray): Predicted probabilities.
+            y (np.ndarray): Actual labels. (True Labels).
+
+        Returns:
+            float: Cross-Entropy Loss.
+        """
+        n = y.shape[1]  # number of examples
+        cost = (-1 / n) * np.sum(y * np.log(y_hat))
+
+        return float(np.squeeze(cost))  # squeeze removes dimensions of size 1
+
+    @staticmethod
+    def mean_squared_error(y_hat: np.ndarray, y: np.ndarray) -> float:
+        """
+        Compute the mean squared error (MSE).
+
+        The mean squared error is defined as:
+
+        MSE = (1/n) Σ (y_i - y_hat_i)^2
+
+        Args:
+            y_hat (np.ndarray): Predicted values.
+            y (np.ndarray): Actual values. (True Labels).
+
+        Returns:
+            float: Mean squared error.
+        """
+        return float(np.mean((y_hat - y) ** 2))

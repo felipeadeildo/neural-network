786942ca517c58c56fd06cf97733388b2215afd1
Felipe Adeildo <felipeadeildo@duck.com>
2024-07-06 17:37:21 -0300

    feat: add init to neural netowork module

---
+++ src/neural_network/__init__.py
@@ -0,0 +1,13 @@
+from .activation_functions import ActivationFunctions
+from .cost_functions import CostFunctions
+from .derivatives import Derivatives
+from .layers import DenseLayer
+from .neural_network import NeuralNetwork
+
+__all__ = [
+    "ActivationFunctions",
+    "CostFunctions",
+    "Derivatives",
+    "DenseLayer",
+    "NeuralNetwork",
+]
